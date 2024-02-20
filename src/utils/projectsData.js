6d997fa2381f8109d817ced9a656a40d76b37494
Nikhil <nikhildhiman9050@gmail.com>
2024-02-21 00:42:44 +0530

    minor fixes and added projects page
    - home page will show only 3 recent blogs instead of 4
    - improved grid and page layout on archives page and projects page

---
+++ src/utils/projectsData.js
@@ -0,0 +1,51 @@
+/**
+ {
+    id: ,
+    title: "",
+    desc: "",
+    repo_link: "",
+    tags: [
+      ""
+    ]
+  },
+ */
+
+export const projectsData = [
+  {
+    id: 1,
+    title: "Bashy",
+    desc: "A minimal looking system fetch script written in bash",
+    repo_link: "https://github.com/bonitoflakez/bashy",
+    tags: [
+      "sh"
+    ]
+  },
+  {
+    id: 2,
+    title: "Portfolio Site",
+    desc: "The site you're seeing right now :P",
+    repo_link: "https://github.com/bonitoflakez/bonitoflakez.github.io",
+    tags: [
+      "astro",
+      "tailwind",
+    ]
+  },
+  {
+    id: 3,
+    title: "GOAT",
+    desc: "A CLI Utility to trace API calls, useful because it prints most of the info directly on the terminal.",
+    repo_link: "https://github.com/bonitoflakez/goat",
+    tags: [
+      "go",
+    ]
+  },
+  {
+    id: 4,
+    title: "goJSON2CLASS",
+    desc: "Converts JSON schema to classes with ease",
+    repo_link: "https://github.com/bonitoflakez/goJSON2CLASS",
+    tags: [
+      "go",
+    ]
+  },
+]
